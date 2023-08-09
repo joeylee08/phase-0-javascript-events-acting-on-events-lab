3f392eab8bd96fd81ca994fda2071b85f5cc8288
Joseph <josephylee72@gmail.com>
2023-08-08 23:08:33 -0700

    completed acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.background = "pink";
+
+function moveDodgerLeft() {
+  const left = +dodger.style.left.replace("px", "");
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+})
+
+function moveDodgerRight() {
+  const left = +dodger.style.left.replace("px", "");
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
